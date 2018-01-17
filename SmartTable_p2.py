f721e6fec08aaeb48461df5a92442106e0a51f2e
Daminda <daminda.12@cse.mrt.ac.lk>
2018-01-17 12:54:13 +0530

    temp

--- SmartTable_p2.py
+++ SmartTable_p2.py
@@ -25,7 +25,7 @@ def addTextOnFrame(imgSrc):														# Add default text on frame and resize 
 	cv2.rectangle(imgTemp,(0,0),(imgTemp.shape[1],30),(0,0,0),-1)
 	cv2.addWeighted(imgTemp,0.5,imgSrc,0.5,0,imgSrc)							# Adding transparent layer
 	cv2.putText(imgSrc, "Press 'q' to Exit", (imgSrc.shape[1]-150,20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
-	imgSrc = cv2.resize(imgSrc, (int(width*1.5),int(height*1.5)))
+	imgSrc = cv2.resize(imgSrc, (int(width*2.1),int(height*1.5)))
 	return imgSrc
 
 
